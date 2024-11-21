d14e6730450512db436bf8ab9919189cded89278
noiseandsmke <emailaccanhbg11@gmail.com>
2024-11-22 01:02:36 +0700

    export files with id project prefix

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,14 +3,13 @@ import { SignUpCard } from "@/features/auth/components/sign-up-card";
 import { redirect } from "next/navigation";
 
 const SignUpPage = async () => {
-  const session = await auth();
+	const session = await auth();
 
-  if (session) {
-    redirect("/");
-  }
+	if (session) {
+		redirect("/");
+	}
 
-  return <SignUpCard />
-      
-}
+	return <SignUpCard />;
+};
 
 export default SignUpPage;
--- src/features/editor/components/ai-sidebar.tsx
+++ src/features/editor/components/ai-sidebar.tsx
@@ -1,83 +1,84 @@
-import { ActiveTool, Editor } from "@/features/editor/types";
-import { cn } from "@/lib/utils";
-import { ToolSidebarHeader } from "@/features/editor/components/tool-sidebar-header";
-import { ToolSidebarClose } from "@/features/editor/components/tool-sidebar-close";
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
 import { useGenerateImage } from "@/features/ai/api/use-generate-image";
-import { useState } from "react";
+import { ToolSidebarClose } from "@/features/editor/components/tool-sidebar-close";
+import { ToolSidebarHeader } from "@/features/editor/components/tool-sidebar-header";
+import { ActiveTool, Editor } from "@/features/editor/types";
 import { usePaywall } from "@/features/subscriptions/hooks/use-paywall";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 interface AiSidebarProps {
-  editor: Editor | undefined;
-  activeTool: ActiveTool;
-  onChangeActiveTool: (tool: ActiveTool) => void;
+	editor: Editor | undefined;
+	activeTool: ActiveTool;
+	onChangeActiveTool: (tool: ActiveTool) => void;
 }
 
 export const AiSidebar = ({
-  editor,
-  activeTool,
-  onChangeActiveTool,
+	editor,
+	activeTool,
+	onChangeActiveTool,
 }: AiSidebarProps) => {
-  const { shouldBlock, triggerPaywall } = usePaywall();
-  const mutation = useGenerateImage();
-  const [value, setValue] = useState("");
+	const { shouldBlock, triggerPaywall } = usePaywall();
+	const mutation = useGenerateImage();
+	const [value, setValue] = useState("");
 
-  const onSubmit = (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
+	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-    if (shouldBlock) {
-      triggerPaywall();
-      return;
-    }
+		if (shouldBlock) {
+			triggerPaywall();
+			return;
+		}
 
-    mutation.mutate({ prompt: value }, {
-      onSuccess: ({ data }) => {
-        editor?.addImage(data);
-      },
-    });
-  }
-  
-  const onClose = () => {
-    onChangeActiveTool("select");
-  }
+		mutation.mutate(
+			{ prompt: value },
+			{
+				onSuccess: ({ data }) => {
+					editor?.addImage(data);
+				},
+			}
+		);
+	};
 
-  return (
-    <aside
-      className={cn(
-        "bg-white relative border-r z-[40] w-[360px] h-full flex flex-col",
-        activeTool === "ai" ? "visible" : "hidden"
-      )}
-    >
-      <ToolSidebarHeader 
-        title="AI"
-        description="Generate an Image using AI"
-      />
-      <ScrollArea>
-        <form className="p-4 space-y-6" onSubmit={onSubmit}>
-          <Textarea 
-            disabled={mutation.isPending}
-            placeholder="Something"
-            cols={30}
-            rows={10}
-            required
-            minLength={3}
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <Button
-            disabled={mutation.isPending}
-            type="submit"
-            className="w-full"
-          >
-            Generate
-          </Button>
-        </form>
-      </ScrollArea>
-      <ToolSidebarClose onClick={onClose}/>
-    </aside>
-  )
+	const onClose = () => {
+		onChangeActiveTool("select");
+	};
+
+	return (
+		<aside
+			className={cn(
+				"bg-white relative border-r z-[40] w-[360px] h-full flex flex-col",
+				activeTool === "ai" ? "visible" : "hidden"
+			)}
+		>
+			<ToolSidebarHeader
+				title="AI"
+				description="Retrieve image project by using LLM"
+			/>
+			<ScrollArea>
+				<form className="p-4 space-y-6" onSubmit={onSubmit}>
+					<Textarea
+						disabled={mutation.isPending}
+						placeholder="Write your prompt here..."
+						cols={30}
+						rows={10}
+						required
+						minLength={3}
+						value={value}
+						onChange={(e) => setValue(e.target.value)}
+					/>
+					<Button
+						disabled={mutation.isPending}
+						type="submit"
+						className="w-full"
+					>
+						Submit
+					</Button>
+				</form>
+			</ScrollArea>
+			<ToolSidebarClose onClick={onClose} />
+		</aside>
+	);
 };
